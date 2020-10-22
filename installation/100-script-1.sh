05fb535b1f7dd11d339980dea9471de7a8fd6ff8
XBC4000 <chrisr414@gmail.com>
2020-10-22 11:15:26 -0400

    Update 100-script-1.sh

--- installation/100-script-1.sh
+++ installation/100-script-1.sh
@@ -61,14 +61,3 @@ cat /etc/pacman.d/mirrorlist
 sudo pacman -Syu
 
 echo "mirrorlist update complete"
-
-#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
-sudo pacman -S --noconfirm --needed git
-  git init
-    git config --global user.name "xbc4000"
-      git config --global user.email "chrisr414@gmail.com"
-        sudo git config --system core.editor nano
-      git config --global credential.helper cache
-    git config --global credential.helper 'cache --timeout=18000'
-  git config --global push.default simple
-echo "Git installed"
