5ca5e5bd66f10f4ce52c35b63d27ca27c5c7d1e0
XBC4000 <chrisr414@gmail.com>
2020-11-22 08:40:40 -0500

    Update 200-script-2.sh

--- installation/200-script-2.sh
+++ installation/200-script-2.sh
@@ -40,11 +40,11 @@ yay -S --noconfirm --needed arj acpi aarchup aconfmgr-git alpm_octopi_utils
     yay -S --noconfirm --needed clipit catfish code chromium chromium-widevine
       yay -S --noconfirm --needed discord
         yay -S --noconfirm --needed evolution etcher-bin
-          yay -S --noconfirm --needed fprintd fingerprint-gui fancontrol-gui
+          yay -S --noconfirm --needed fancontrol-gui
             yay -S --noconfirm --needed gotop github-desktop-bin google-earth-pro galculator guake
               yay -S --noconfirm --needed hexchat hddtemp
-                yay -S --noconfirm --needed inxi input-wacom-dkms
-                  yay -S --noconfirm --needed lm_sensors libfprint lsb-release libreoffice-still lib32-libevdev libwacom
+                yay -S --noconfirm --needed inxi
+                  yay -S --noconfirm --needed lm_sensors libfprint lsb-release libreoffice-still lib32-libevdev
                     yay -S --noconfirm --needed materia-gtk-theme microsoft-edge-dev
                     yay -S --noconfirm --needed netflix-qdesktop notify-osd npm
                   yay -S --noconfirm --needed onboard pod2man octopi octopi-notifier-qt5
@@ -52,24 +52,13 @@ yay -S --noconfirm --needed arj acpi aarchup aconfmgr-git alpm_octopi_utils
               yay -S --noconfirm --needed qbittorrent
             yay -S --noconfirm --needed radiotray redshift
           yay -S --noconfirm --needed sharutils sl safeeyes spotify spotifywm-git
-        yay -S --noconfirm --needed telegram-desktop tp_smapi thinkfan tlp tlpui-git tomboy-ng-bin tor-browser tk tor tint2
+        yay -S --noconfirm --needed telegram-desktop tp_smapi tlp tlpui-git tomboy-ng-bin tor-browser tk tor tint2
       yay -S --noconfirm --needed unclutter uudeview
     yay -S --noconfirm --needed vpn-unlimited vnstat
-  yay -S --noconfirm --needed xf86-input-wacom
+  yay -S --noconfirm --needed 
 yay -S --noconfirm --needed youtube-desktop
 
   echo "Yay packages installed"
-
-modprobe thinkpad_acpi
-cat /proc/acpi/ibm/fan
-
-  echo "Modules enabled"
-
-sudo systemctl enable thinkfan.service
-sudo systemctl start thinkfan.service
-echo level full_speed > /proc/acpi/ibm/fan
-
-  echo "Thinkfan enabled"
 
 sudo systemctl enable tlp.service
   sudo systemctl mask systemd-rfkill.service
